9fc354b7cd3e6515669f47f0a6ea8472dbaa787b
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-06-28 14:33:23 +1200

    mobile adjustment

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const shareBtn = document.getElementById("share");
+const shareBtn = document.getElementById("share-button");
 const socials = document.getElementById("socials");
+
 
 shareBtn.addEventListener("click", () => {
   socials.classList.toggle("show");
-    shareBtn.classList.toggle("active");
+  shareBtn.classList.toggle("active");
+
 });
