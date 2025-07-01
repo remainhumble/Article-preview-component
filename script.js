7b82c5d613ba858f2992abd06ed0c855ed47018f
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-07-01 21:49:08 +1200

    hard work desktop adjustment

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const shareBtn = document.getElementById("share-button");
 const socials = document.getElementById("socials");
 
-
 shareBtn.addEventListener("click", () => {
   socials.classList.toggle("show");
   shareBtn.classList.toggle("active");
-
 });
